874e77007cc733f2642b519e74bbfc4667c462ec
hugo matos <hugocfmatos@gmail.com>
2020-09-09 19:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays() {
+return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday,name) {
+  return `Happy ${holiday} to ${name}`
+}
+function holidatCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
+}
